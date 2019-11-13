dddb709031f09c66848877522e3d82dd17d8ebb5
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2019-11-13 08:45:55 -0300

    IFPRAcesso V1.0

--- ifpracessomain/static/ifpracessomain/js/relatorio.js
+++ ifpracessomain/static/ifpracessomain/js/relatorio.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    // var doc = new jsPDF();
-    // $('.exportar').click(function () {   
-    //     doc.fromHTML($('.content').html(), 15, 15, {
-    //         'width': 170,
-    //     });
-    //     doc.save('sample-file.pdf');
-    // });
     $('.exportar').click(function CreatePDFfromHTML() {
         let doc = new jsPDF('p','mm','a4');
         doc.addHTML($('.html-content'),function() {
